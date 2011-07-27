2387006752e951066c68a3a8619ab07a7ad9bd0c
Tag Ashby <tgashby@calpoly.edu>
2011-07-27 13:37:53 -0700

    It works!~ Sweeeet. Moved the SDL window, GLContext, and texture loading into the other thread, silly me for not doing that in the first place. Sadly I had to remove the ability to change the texture image. So now you'll just have to restart the program...

--- src/AnimationCalibrator.cpp
+++ src/AnimationCalibrator.cpp
@@ -10,7 +10,6 @@ void deleteFrame();
 void clearFrames();
 void viewFrame();
 void playAnimation();
-void resetTexture();
 void setDimensions();
 void setDelay();
 void printFrames();
@@ -19,85 +18,31 @@ void printFrames();
 int handleAnimation(void*);
 
 SDL_Window* window;
+SDL_GLContext glContext;
 TGA::Texture* texture;
 TGA::Animation animation;
 int SCREEN_WIDTH = 1024;
 int SCREEN_HEIGHT = 512;
 
-bool running;
+bool running, textureLoaded;
+SDL_Thread* animThread;
 
 int main(int argc, char **argv)
 {
 	texture = new TGA::Texture();
 
-	std::string fileName;
-
 	char command;
-
-	// Create a SDL_Thread*
-	SDL_Thread* animThread;
 
 	std::cout << "Animation Calibrator:\n" << "A program to run through animations and tweak frame delays and settings.\n\n"
 		<< "Please have your animation in a single file with frames laid out however you want.\n"; 
 
-	std::cout << "Please enter the file name of the image containing your animation: ";
-
-	getline(std::cin, fileName);
-
-	//////////////////////////////////////////////////////////////////////////
-	// Initialize the SDL window and GL context and all that stuff
-	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
-
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
-	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-	window = SDL_CreateWindow("Animation-Calibrator", SDL_WINDOWPOS_CENTERED, 
-		SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-
-	SDL_GLContext glContext = SDL_GL_CreateContext(window);
-	//////////////////////////////////////////////////////////////////////////
-
-	//////////////////////////////////////////////////////////////////////////
-	// Initialize all the OGL stuff
-	glMatrixMode(GL_PROJECTION);
-
-	glLoadIdentity();
-
-	glOrtho(0.0, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0, 0.0, 1000.0);
-
-	glDisable(GL_DEPTH_TEST);
-
-	glClearColor(1, 1, 1, 1);
-
-	glShadeModel(GL_SMOOTH);
-
-	glMatrixMode(GL_MODELVIEW);
-
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	glLoadIdentity();
-	//////////////////////////////////////////////////////////////////////////
-
-	// WHILE the texture hasn't loaded successfully
-	while(!texture->loadTexture(fileName))
-	{
-		std::cout << "Enter the name of the image to try: ";
-
-		getline(std::cin, fileName);
-	}
-
-	animation.setTexture(texture);
-
 	running = true;
+	textureLoaded = false;
 
 	// Start the handleAnimation thread! (YAY!)
 	animThread = SDL_CreateThread(handleAnimation, "Anim Thread");
+
+	while(!textureLoaded);
 
 	displayMenu();
 
@@ -128,10 +73,6 @@ int main(int argc, char **argv)
 				setDelay();
 				break;
 
-			case 'T':
-				resetTexture();
-				break;
-
 			case 'P':
 				playAnimation();
 				break;
@@ -149,10 +90,6 @@ int main(int argc, char **argv)
 				break;
 
 			case 'Q':
-				SDL_GL_DeleteContext(glContext);
-
-				SDL_DestroyWindow(window);
-
 				running = false;
 				break;
 
@@ -179,14 +116,11 @@ void displayMenu()
 		<< "X - clear all frames\n"
 		<< "C - change a frame's dimensions\n"
 		<< "R - set the delay of a frame\n"
-		<< "T - change the animation image\n"
 		<< "P - play the entire animation\n"
 		<< "Y - print all frames\n"
 		<< "V - view a single frame\n"
 		<< "M - display this menu\n"
 		<< "Q - quit the program\n";
-
-	std::cout << "SDL ERROR: " << SDL_GetError() << std::endl;
 }
 
 void addFrame()
@@ -288,27 +222,6 @@ void playAnimation()
 	std::cout << "Hope you like it!\n";
 }
 
-void resetTexture()
-{
-	std::cout << "Enter a new file name for the animation image: ";
-
-	std::string fileName;
-	getline(std::cin, fileName);
-		
-	// WHILE the texture hasn't loaded correctly
-	while(!(texture->loadTexture(fileName)))
-	{
-		std::cout << "Enter the name of the image to try: ";
-
-		getline(std::cin, fileName);
-	}
-
-	animation.setTexture(texture);
-	animation.clearFrames();
-
-	std::cout << "Texture reset and all frames removed.\n";
-}
-
 void setDimensions()
 {
 	SDL_Rect newRect;
@@ -357,18 +270,81 @@ void printFrames()
 
 int handleAnimation(void* unused)
 {
-	// WHILE the program is still running
+	//////////////////////////////////////////////////////////////////////////
+	// Initialize the SDL window and GL context and all that stuff
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	window = SDL_CreateWindow("Animation-Calibrator", SDL_WINDOWPOS_CENTERED, 
+		SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+	glContext = SDL_GL_CreateContext(window);
+	//////////////////////////////////////////////////////////////////////////
+
+	//////////////////////////////////////////////////////////////////////////
+	// Initialize all the OGL stuff
+	glMatrixMode(GL_PROJECTION);
+
+	glLoadIdentity();
+
+	glOrtho(0.0, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0, 0.0, 1000.0);
+
+	glDisable(GL_DEPTH_TEST);
+
+	glClearColor(1, 1, 1, 1);
+
+	glShadeModel(GL_SMOOTH);
+
+	glMatrixMode(GL_MODELVIEW);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glLoadIdentity();
+	//////////////////////////////////////////////////////////////////////////
+	
+	std::string fileName;
+
+	std::cout << "Please enter the file name of the image containing your animation: ";
+
+	getline(std::cin, fileName);
+
+	// WHILE the texture hasn't loaded successfully
+	while(!texture->loadTexture(fileName))
+	{
+		std::cout << "Enter the name of the image to try: ";
+
+		getline(std::cin, fileName);
+	}
+
+	animation.setTexture(texture);
+
+	textureLoaded = true;
+
 	while(running)
 	{
-		// Update the animation
 		animation.update();
 
-		// Draw the animation
-		animation.draw(SCREEN_WIDTH / 2 - texture->getWidth() / 2, 
-		 SCREEN_HEIGHT / 2 - texture->getHeight() /2);
+		if(!animation.isDone())
+		{
+
+			animation.draw((GLfloat)SCREEN_WIDTH / 2 - texture->getWidth(), 
+				(GLfloat)SCREEN_HEIGHT / 2 - texture->getHeight());
+		}
 
 		SDL_GL_SwapWindow(window);
 	}
+
+	SDL_GL_DeleteContext(glContext);
+
+	SDL_DestroyWindow(window);
 
 	return 0;
 }
